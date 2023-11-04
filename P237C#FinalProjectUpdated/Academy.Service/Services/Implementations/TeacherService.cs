cdc1a37d568c43d33f08e745d7077242dd740ffe
Nihattaghizade <nihattagizade2003@gmail.com>
2023-11-05 01:10:14 +0400

    Commit 6

--- P237C#FinalProjectUpdated/Academy.Service/Services/Implementations/TeacherService.cs
+++ P237C#FinalProjectUpdated/Academy.Service/Services/Implementations/TeacherService.cs
@@ -31,24 +31,63 @@ namespace Academy.Service.Services.Implementations
             return "Successfully created";
         }
 
-        public Task GetAllAsync()
+        public async Task GetAllAsync()
         {
-            throw new NotImplementedException();
+            List<Teacher> teachers = await _teacherRepository.GetAllAsync();
+            foreach (Teacher teacher in teachers)
+            {
+                Console.WriteLine($"Id: {teacher.Id} Name: {teacher.Name} Surname: {teacher.SurName} Username: {teacher.Username} Password: {teacher.Password} Salary:{teacher.Salary} Age: {teacher.Age} Educationtype: {teacher.EducationType} CreatedAt: {teacher.CreatedAt} UpdatedAt: {teacher.UpdatedAt}");
+            }
         }
 
-        public Task<string> GetAsync(string id)
+        public async Task<string> GetAsync(string id)
         {
-            throw new NotImplementedException();
+            Teacher teacher = await _teacherRepository.GetAsync(x => x.Id == id);
+            if (teacher == null)
+                return "Teacher not found";
+
+            Console.WriteLine($"Id: {teacher.Id} Name: {teacher.Name} Surname: {teacher.SurName} Username: {teacher.Username} Password: {teacher.Password} Salary:{teacher.Salary} Age: {teacher.Age} Educationtype: {teacher.EducationType} CreatedAt: {teacher.CreatedAt} UpdatedAt: {teacher.UpdatedAt}");
+            return "Success";
         }
 
-        public Task<string> RemoveAsync(string id)
+        public async Task<string> RemoveAsync(string id)
         {
-            throw new NotImplementedException();
+            Teacher teacher = await _teacherRepository.GetAsync(x => x.Id == id);
+            if (teacher == null)
+                return "Teacher not found";
+
+            await _teacherRepository.RemoveAsync(teacher);
+            return "Removed successfully";
         }
 
-        public Task<string> UpdateAsync(string id, string name, string surName, string userName, string password, double salary, int age, EducationType educationType)
+        public async Task<string> UpdateAsync(string id, string name, string surName, string userName, string password, double salary, int age, EducationType educationType)
         {
-            throw new NotImplementedException();
+            Teacher teacher = await _teacherRepository.GetAsync(x => x.Id == id);
+
+            if (teacher == null)
+                return "Teacher not found";
+            if (string.IsNullOrWhiteSpace(name))
+                return "Name can't be empty";
+            if (string.IsNullOrWhiteSpace(surName))
+                return "Surname can't be empty";
+            if (string.IsNullOrWhiteSpace(userName))
+                return "Username can't be empty";
+            if (string.IsNullOrWhiteSpace(password))
+                return "Password can't be empty";
+            if (salary <= 0 || salary > 10000)
+                return "Salary can be more than 0 or less than 10000";
+            if (age <= 0 || age > 65)
+                return "Age can be more than 0 or less than 65";
+
+            teacher.Name = name;
+            teacher.SurName = surName;
+            teacher.Username = userName;
+            teacher.Password = password;
+            teacher.Salary = salary;
+            teacher.Age = age;
+            teacher.EducationType = educationType;
+            teacher.UpdatedAt = DateTime.UtcNow.AddHours(4);
+            return "Updated successfully";
         }
     }
 }
