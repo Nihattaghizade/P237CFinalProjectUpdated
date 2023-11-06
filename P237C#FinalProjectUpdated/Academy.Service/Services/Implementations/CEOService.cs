f22c4909792b82df147c7bf6ff9a64595f96f453
Nihattaghizade <nihattagizade2003@gmail.com>
2023-11-06 13:28:29 +0400

    Commit 10

---
+++ P237C#FinalProjectUpdated/Academy.Service/Services/Implementations/CEOService.cs
@@ -0,0 +1,40 @@
+﻿using Academy.Service.Services.Interfaces;
+
+namespace Academy.Service.Services.Implementations
+{
+    public class CEOService : IUserService
+    {
+        bool IsLogin = false;
+        public async Task<bool> Authenticated()
+        {
+            return IsLogin;
+        }
+
+        public async Task SignIn()
+        {
+            Console.WriteLine("Add username");
+            string Username = Console.ReadLine();
+            Console.WriteLine("Add password");
+            string Password = Console.ReadLine();
+
+            if (Username == "Nihattt" && Password == "Nihat123")
+            {
+                Console.WriteLine("Username or password is incorrect");
+            }
+            else
+            {
+                IsLogin = true;
+            }
+            Console.WriteLine("Signed in successfully");
+        }
+
+        public async Task SignOut()
+        {
+            if (IsLogin == true)
+            {
+                IsLogin = false;
+            }
+            Console.WriteLine("Signed out successfully");
+        }
+    }
+}
--- P237C#FinalProjectUpdated/Academy.Service/Services/Implementations/MenuService.cs
+++ P237C#FinalProjectUpdated/Academy.Service/Services/Implementations/MenuService.cs
@@ -1,6 +1,38 @@
-﻿namespace Academy.Service.Services.Implementations
+﻿using Academy.Service.Services.Interfaces;
+
+namespace Academy.Service.Services.Implementations
 {
     public class MenuService
     {
+        CEOService ceoService = new CEOService();
+        StudentService studentService = new StudentService();
+        TeacherService teacherService = new TeacherService();
+        DeveloperService developerService = new DeveloperService();
+
+
+
+
+
+
+
+
+
+
+
+
+
+        //async Task Menu()
+        //{
+        //    if (!userService.Authenticated())
+        //    {
+        //        Console.WriteLine("1. SignIn");
+        //    }
+        //    else
+        //    {
+        //        Console.WriteLine("2. SignOut");
+        //        Console.WriteLine("3. Get all users");
+        //    }
+        //    Console.WriteLine("0. Close");
+        //}
     }
 }
